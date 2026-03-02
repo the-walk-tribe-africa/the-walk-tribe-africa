64f07743822418c4ff42fb3af2b34059753c82de
Brian Kipkirui <126571063+Br1an-K1pk1ru1@users.noreply.github.com>
2026-03-02 08:42:06 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,10 @@
-
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open("walk-cache").then(function(cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+      ]);
+    })
+  );
+});
